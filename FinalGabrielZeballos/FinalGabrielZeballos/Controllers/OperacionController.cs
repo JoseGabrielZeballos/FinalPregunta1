3ae4fb99039ae25465eb5a7b6360764562e2030f
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2020-12-12 12:45:25 -0400

    Pregunta1

---
+++ FinalGabrielZeballos/FinalGabrielZeballos/Controllers/OperacionController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace FinalGabrielZeballos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperacionController : ControllerBase
+    {
+        [HttpGet]
+        public string Ejercicio1(int numero)
+        {
+            string frase = "";
+            if (numero < 0)
+            { frase = "ERROR"; }
+            if (numero == 0)
+            { frase = "Realizado por Gabriel "; }
+            if (numero > 0)
+            { frase = "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg"; }
+            return frase;
+
+
+        }
+    }
+}
+    
